d6de294e232f657409b2648d85051c19f4f15b01
Marko Galavić <147241279+Marko455@users.noreply.github.com>
2025-11-01 12:36:35 +0100

    Nino Telefonino
    Marko Galavić - Nino Telefonino

--- MarkoGNinoTel/ninoTel.py
+++ MarkoGNinoTel/ninoTel.py
@@ -1,33 +1,66 @@
 import re
 
-def validiraj_broj_telefona(broj: str) -> str | None:
-    broj = broj.strip()
+def validiraj_broj_telefona() -> dict:
+    broj = input("Unesite telefonski broj: ").strip()
 
     if broj.startswith('+'):
         cisti = '+' + re.sub(r'\D', '', broj[1:])
     else:
         cisti = re.sub(r'\D', '', broj)
 
-    dopusteni_pozivni = [
-        '01', '020', '021', '022', '023', '031', '032', '033', '034', '035',
-        '040', '042', '043', '044', '047', '048', '049','051', '052', '053', 
-        '091', '092', '095', '097', '098', '099', '0800'
-    ]
-
-    dopustena_mjesta = [
-        'Grad Zagreb i Zagrebacka zupanija', "Dubrovacko-neretvanska zupanija", "Splitsko-dalmatinska zupanija", "Sibensko-kninska zupanija", "Zadarska zupanija",
-        "Osjecko-baranjska zupanija", "Vukovarsko-srijemska zupanija", "Viroviticko-podravska zupanija", "Pozesko-slavonska zupanija", "Brodsko-posavska zupanija",
-        "Medimurska zupanija", "Varazdinska zupanija", "Bjelovarsko-bilogorska zupanija", "Sisacko-moslavacka zupanija", "Karlovacka zupanija", "Koprivnicko-krizevacka zupanija",
-        "Krapinsko-zagorska zupanija", "Primorsko-goranska zupanija", "Istarska zupanija", "Licko-senjska zupanija", "A1 Hrvatska", "Tomato", "Telemach", "bonbon",
-        "Hrvatsko Telekom", "Besplatni pozivi", "Komercijalni pozivi", "Glasovanje telefonom", "Usluge s neprimjerenim sadrzajem", "Nagradne igre", "Usluge namjenjene djeci",
-        "jedinstveni pristupno broj za cijelu drzavu za posebne usluge"
-    ]
+    pozivni_mjesta = {
+        '01':  'Grad Zagreb i Zagrebačka županija',
+        '020': 'Dubrovačko-neretvanska županija',
+        '021': 'Splitsko-dalmatinska županija',
+        '022': 'Šibensko-kninska županija',
+        '023': 'Zadarska županija',
+        '031': 'Osječko-baranjska županija',
+        '032': 'Vukovarsko-srijemska županija',
+        '033': 'Virovitičko-podravska županija',
+        '034': 'Požeško-slavonska županija',
+        '035': 'Brodsko-posavska županija',
+        '040': 'Međimurska županija',
+        '042': 'Varaždinska županija',
+        '043': 'Bjelovarsko-bilogorska županija',
+        '044': 'Sisačko-moslavačka županija',
+        '047': 'Karlovačka županija',
+        '048': 'Koprivničko-križevačka županija',
+        '049': 'Krapinsko-zagorska županija',
+        '051': 'Primorsko-goranska županija',
+        '052': 'Istarska županija',
+        '053': 'Ličko-senjska županija',
+        '091': 'Hrvatski Telekom (T-Mobile / bonbon)',
+        '092': 'Telemach',
+        '095': 'A1 Hrvatska (bivši Vipnet)',
+        '097': 'Telemach (bivši Tele2)',
+        '098': 'Hrvatski Telekom (T-Mobile)',
+        '099': 'A1 Hrvatska (bivši Vipnet)',
+        '0800': 'Besplatni pozivi (posebne usluge)',
+    }
 
     lokalni = cisti
     if cisti.startswith('+385'):
         lokalni = '0' + cisti[4:]
 
-    if any(lokalni.startswith(p) for p in dopusteni_pozivni):
-        return cisti
-    else:
-        return None
+    pozivni = None
+    mjesto = None
+
+    for p in sorted(pozivni_mjesta.keys(), key=len, reverse=True):
+        if lokalni.startswith(p):
+            pozivni = p
+            mjesto = pozivni_mjesta[p]
+            break
+
+    rezultat = {
+        "broj": cisti,
+        "pozivni": pozivni,
+        "mjesto": mjesto,
+        "validan": pozivni is not None
+    }
+
+    return rezultat
+
+if __name__ == "__main__":
+    rezultat = validiraj_broj_telefona()
+    print("\nRezultat validacije:")
+    print(rezultat)
